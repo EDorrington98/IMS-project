593cc5cf5f1d5a71b174140a65f41ebb9605c619
Elliott <elliott.98@hotmail.com>
2020-02-13 23:41:30 +0000

    fixed alot of bugs, completed README

--- src/main/java/com/qa/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/persistence/dao/ItemDaoMysql.java
@@ -1,72 +1,73 @@
 package com.qa.persistence.dao;
 
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.ResultSet;
-	import java.sql.Statement;
-	import java.util.ArrayList;
-	import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-	import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 
-	import com.qa.controller.ItemController;
-	import com.qa.persistence.domain.Item;
-	import com.qa.utils.Config;
+import com.qa.controller.ItemController;
+import com.qa.persistence.domain.Item;
+import com.qa.utils.Config;
 
-	public class ItemDaoMysql implements Dao<Item> {
-		
-		public static final Logger logger = Logger.getLogger(ItemDaoMysql.class);
-		
-		public List<Item> readAll() {
-			ArrayList<Item> items = new ArrayList<Item>();
-			try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)) {
+public class ItemDaoMysql implements Dao<Item> {
+
+	public static final Logger logger = Logger.getLogger(ItemDaoMysql.class);
+
+	public List<Item> readAll() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password);
 				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery("select * from items");
-				while (resultSet.next()) {
-					int id = resultSet.getInt("id");
-					String name = resultSet.getString("name");
-					int value = resultSet.getInt("value");
-					Item item = new Item(id, name, value);
-					items.add(item);
-				}
-			} catch (Exception e) {
-				logger.error(e.getMessage());
+				ResultSet resultSet = statement.executeQuery("select * from items");) {
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				int value = resultSet.getInt("value");
+				Item item = new Item(id, name, value);
+				items.add(item);
 			}
-			return items;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
 		}
-
-		public void create(Item item) {
-			try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-				Statement statement = connection.createStatement();
-				statement.executeUpdate("INSERT into items(name, value) values('" + item.getName() + "','" + item.getValue()+"')");
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-				
-			} 
-		}
-
-		public void update(int id, Item item) {
-			try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-				Statement statement = connection.createStatement();
-				statement.executeUpdate("UPDATE items SET name ='" + item.getName() + "' , value = '" + item.getValue() + "' WHERE id =" + id);
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		}
-
-		public void delete(int id) {
-			try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-				Statement statement = connection.createStatement();
-				statement.executeUpdate("DELETE from items WHERE id =" + id);
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		}
-
-
-
-
-
+		return items;
 	}
 
+	public void create(Item item) {
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
 
+			statement.executeUpdate(
+					"INSERT into items(name, value) values('" + item.getName() + "','" + item.getValue() + "')");
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+
+		}
+	}
+
+	public void update(int id, Item item) {
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
+
+			statement.executeUpdate("UPDATE items SET name ='" + item.getName() + "' , value = '" + item.getValue()
+					+ "' WHERE id =" + id);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+	public void delete(int id) {
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
+
+			statement.executeUpdate("DELETE from items WHERE id =" + id);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+}
--- src/main/java/com/qa/persistence/dao/OrderDaoMysql.java
+++ src/main/java/com/qa/persistence/dao/OrderDaoMysql.java
@@ -14,14 +14,16 @@ import com.qa.persistence.domain.Order;
 import com.qa.utils.Config;
 
 public class OrderDaoMysql implements Dao<Order> {
-	
+
 	public static final Logger logger = Logger.getLogger(OrderController.class);
-	
+
 	public List<Order> readAll() {
 		ArrayList<Order> orders = new ArrayList<Order>();
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)) {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from orders");
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from orders");) {
+
 			while (resultSet.next()) {
 				int id = resultSet.getInt("id");
 				int customerId = resultSet.getInt("customers_id");
@@ -36,35 +38,35 @@ public class OrderDaoMysql implements Dao<Order> {
 	}
 
 	public void create(Order order) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-			Statement statement = connection.createStatement();
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
+
 			statement.executeUpdate("INSERT into orders(customers_id) values('" + order.getCustomerId() + "')");
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 
-		} 
+		}
 	}
 
 	public void update(int id, Order order) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-			Statement statement = connection.createStatement();
-			statement.executeUpdate("UPDATE orders SET name ='" + order.getCustomerId() + "' , value = '" + order.getCost() + "' WHERE id =" + id);
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
+
+			statement.executeUpdate("UPDATE orders SET name ='" + order.getCustomerId() + "' , value = '"
+					+ order.getCost() + "' WHERE id =" + id);
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
 	}
 
 	public void delete(int id) {
-		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db", Config.username, Config.password)){
-			Statement statement = connection.createStatement();
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://35.234.153.182:3306/project_db",
+				Config.username, Config.password); Statement statement = connection.createStatement();) {
+
 			statement.executeUpdate("DELETE from orders WHERE id =" + id);
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		}
 	}
-
-
-
-
 
 }
